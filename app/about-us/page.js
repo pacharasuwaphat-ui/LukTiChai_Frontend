d924dc0aaabc7252ab5a2f992fbab03ebd7bdd97
Pachara Yonyu <pacharasuwaphat@gmail.com>
2026-03-28 16:49:04 +0700

    make payment(for dev) system use omise test mode

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -41,7 +41,7 @@ const [open, setOpen] = useState(false);
             <li><Link className="nav-link" href="/home">Home</Link></li>
             <li><Link className="nav-link" href="/home/history">History</Link></li>
             <li><Link className="nav-link" href="/fortune/card">เปิดไพ่</Link></li>
-            <li><Link className="nav-link" href="/fortune-telling">ดูดวงวัน/เดือน/ปีเกิด</Link></li>
+            <li><Link className="nav-link" href="/fortune/d-m-y">ดูดวงวัน/เดือน/ปีเกิด</Link></li>
             <li><Link className="nav-link" href="/fortune/siamsi">สุ่มเซียมซี</Link></li>
             <li><Link className="nav-link" href="/fortune/disc">ลูกเต๋าพยากรณ์</Link></li>
             <li><a className="nav-link" href="/fortune/phone">ทำนายเบอร์โทร</a></li>
